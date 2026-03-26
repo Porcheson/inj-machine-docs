74626b54db5e75c86ba1e420d3f639552de43dcc
jingshui127 <jingshui127@163.com>
2026-03-26 11:19:03 +0800

    修复 functions.ts 文件中的语法错误

--- docs/.vitepress/theme/untils/functions.ts
+++ docs/.vitepress/theme/untils/functions.ts
@@ -1,5 +1,4 @@
-const pattern
-    = /[a-zA-Z0-9_\u0392-\u03C9\u00C0-\u00FF\u0600-\u06FF\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4DBF\uF900-\uFAFF\u3040-\u309F\uAC00-\uD7AF]+/g
+const pattern = /[a-zA-Z0-9_\u0392-\u03C9\u00C0-\u00FF\u0600-\u06FF\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4DBF\uF900-\uFAFF\u3040-\u309F\uAC00-\uD7AF]+/g
 
 export function countWord(data: string) {
     const m = data.match(pattern)
